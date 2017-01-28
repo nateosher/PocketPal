682d2e6b85303b604413843f81508eec33a17e03
Nate Osher <Nateoshe@gmail.com>
2017-01-27 23:24:26 -0600

    Basic monitoring of changes in number of articles

--- pocket.py
+++ pocket.py
@@ -3,6 +3,7 @@
 
 import requests
 import json
+from Change import CalculateChange
 from datetime import datetime
 from os import path
 
@@ -19,17 +20,22 @@ if not path.isfile("secret.py"):
 else:
 	import secret
 
+	# Get the last date as an array of ints
 	last_date = []
 	data = open('datafile.txt','r')
 	for datum in data:
-		last_date.append(datum.rstrip())
+		last_date.append(int(datum.rstrip()))
 	data.close()
 	print last_date
+
+	# Get current date as list of ints
 	current_date = list(datetime.now().timetuple()[0:6])
-	print current_date
+
+	# Update last date
 	data = open('datafile.txt', 'w')
-	for i in range(0,6):
+	for i in range(6):
 		data.write(str(current_date[i]) + "\n")
+	# Make request to see how many articles user has
 	url = "https://getpocket.com/v3/get"
 	info = {"consumer_key": secret.consumer_key, "access_token" : secret.access_token}
 	res = requests.post(url, data=info)
@@ -39,4 +45,11 @@ else:
 	for key in results['list']:
 		counter += 1
 
+	data.write(str(counter))
+	data.close()
+	current_date.append(counter)
+	print current_date
+
 	print "You have " + str(counter) + " saved articles"
+	change_message = CalculateChange(last_date, current_date)
+	print change_message
