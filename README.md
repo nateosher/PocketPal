d44d1ab427bf5b91640f5eeae3e32f42847fff99
Nate Osher <Nateoshe@gmail.com>
2017-07-31 19:09:58 -0400

    Edit readme, add preliminary stats functionality

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# pocket
+# Pocket
 Simple command-line app for monitoring pocket usage/auto updating article list using Pocket's API. 
 
-Written in Python. 
+Note that as of now this is not really an application so much as a tool for me personally to better manage my Pocket account, and a fun personal project. 
--- stats.py
+++ stats.py
@@ -1,6 +1,40 @@
 # stats.py 
 # for storing methods relating to collecting stats on reading habits
 
-def Reading_Stats(poc):
+def ReadingStats(poc):
+	sites = {}
+	words = []
 	archived = poc.get_archived()
+	for key in archived.keys():
+		cur = archived[key]
+		words.append(int(cur["word_count"]))
+		urlStem = cur["resolved_url"].split("/")[2]
+		urlStem = urlStem.split(".")
+		if len(urlStem) == 2:
+			urlStem = urlStem[0]
+		else:
+			urlStem = urlStem[1]
+
+		if urlStem in sites:
+			sites[urlStem] += 1
+		else:
+			sites[urlStem] = 1
+
+	# Get top 5 sites
+	top5 = sorted(sites, key=sites.get, reverse=True)[:5]
+	print "Top 5 news sources"
+	for key in top5:
+		print key + " : " + str(sites[key])
+
+	print ""
+
+	print "Average # of words per article: " + str(sum(words)/len(words))
+	print ""
 	return 0
+
+if __name__ == '__main__':
+	import secret
+	from Pocket import Pocket
+	poc = Pocket(secret.consumer_key, secret.access_token)
+
+	ReadingStats(poc)
